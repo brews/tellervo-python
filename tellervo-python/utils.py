1d252df49b5a2ba0eed898174f86377ca814f856
Brewster Malevich <sbmalev@gmail.com>
2015-10-09 14:15:05 -0700

    Rename files for repo rename

--- tellervo-python/utils.py
+++ tellervo-python/utils.py
@@ -7,7 +7,6 @@ def md5hash(s):
     """MD5 hash a string, return hex"""
     return md5(s.encode('utf-8')).hexdigest()
 
-
 def make_validated_parser(schemafile):
     """Create a validated XML parser from input schema file/URL"""
     xml_schema = etree.XMLSchema(file = schemafile)
